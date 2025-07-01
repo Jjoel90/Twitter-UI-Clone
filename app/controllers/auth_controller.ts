9e5e2f3f2ec100428092e6085e2c7743740486dc
Jjoel90 <joelilunga090@gmail.com>
2025-07-01 15:02:45 +0200

    ajout des routes affichage et actions

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -1,4 +1,19 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import { Template } from 'edge.js'
 
 export default class AuthController {
+    public async registerShow({ view }: HttpContext) {
+
+        return view.render('auth/register')
+    }
+
+    public async loginShow({ view }: HttpContext) {
+
+        return view.render('auth/logout')
+    }
+
+    public async logoutShow({ view }: HttpContext) {
+
+        return view.render('auth/logout')
+    }
 }
--- start/routes.ts
+++ start/routes.ts
@@ -7,11 +7,20 @@
 |
 */
 
-import BlogController from '#controllers/BlogController'
 import router from '@adonisjs/core/services/router'
+import BlogController from '#controllers/BlogController'
+import AuthController from '#controllers/auth_controller'
 
 router.on('/').render('pages/home')
-router.post('/register', 'AuthController.register')
-router.post('/login', 'AuthController.login')
-router.post('/logout','AuthController.logout')
+
+//Routes pour afficher les formulaires (GET)
+router.get('/register', [AuthController, 'registerShow'])
+router.get('/login', [AuthController, 'loginShow'])
+router.get('/logout', [AuthController, 'logoutShow'])
+
+// Routes qui vont passer a l'actions (POST)
+router.post('/register', [AuthController, 'registerShow'])
+router.post('/login', [AuthController, 'loginShow'])
+router.post('/logout', [AuthController, 'logoutShow'])
+
 
