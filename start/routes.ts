2ca121d5714d7ce3267e2fc3f547edc19c0d4ba9
Jjoel90 <joelilunga090@gmail.com>
2025-07-01 14:09:14 +0200

    creation de l'authetification

--- start/routes.ts
+++ start/routes.ts
@@ -11,4 +11,7 @@ import BlogController from '#controllers/BlogController'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+router.post('/register', 'AuthController.register')
+router.post('/login', 'AuthController.login')
+router.post('/logout','AuthController.logout')
 
